8239933296d12457394f6303759ae6bdd9d95e52
flaviosampaioreisdelima <flaviosampaioreisdelima@172.17.204.204>
2016-06-19 16:53:19 -0300

    Commit Flávio - Adicionado o Bean Cliente.java - 30/05/2016

--- src/java/br/com/minaciolog/gerenciador/servlet/Controller.java
+++ src/java/br/com/minaciolog/gerenciador/servlet/Controller.java
@@ -8,6 +8,7 @@ package br.com.minaciolog.gerenciador.servlet;
 import java.io.IOException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,6 +17,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author flaviosampaioreisdelima
  */
+@WebServlet(urlPatterns="/Executa")
 public class Controller extends HttpServlet {
 
     @Override
