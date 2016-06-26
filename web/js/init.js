c75941123ccd5b889ba5a2fb0a41c5a138ae8567
flaviosampaioreisdelima <flaviosampaioreisdelima@172.17.204.204>
2016-06-25 22:24:03 -0300

    Realizado a inclusão das funções JavaScript para carregar o Modal.

--- web/js/init.js
+++ web/js/init.js
@@ -13,5 +13,20 @@ $(document).ready(function(){
     $('.scrollspy').scrollSpy();
     
     $('.tabs-wrapper').pushpin({ top: $('.tabs-wrapper').offset().top });
+    
+    $("#BotaoAdicionar").click(function(){
+        $("#SessaoIncluir").show();
+    });
+    
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      opacity: .5, // Opacity of modal background
+      in_duration: 300, // Transition in duration
+      out_duration: 200, // Transition out duration
+    }
+  );
+        
+    $('select').material_select();
 
   });
